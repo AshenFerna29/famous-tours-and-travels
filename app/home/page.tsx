2620f03c3f8d06ad40df68ebb4a12038481a35f1
Kaveesha Sulakshana <kaveeshasulakshana33@gmail.com>
2025-07-29 03:11:19 +0530

    splash screen smooth transistion

---
+++ app/home/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar';
+import Hero from '@/components/Hero';
+import WelcomeSection from '@/components/WelcomeSection';
+
+export default function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <WelcomeSection />
+    </>
+  );
+}
