50f11741bb3f42b9eb4cb5c4d3310da166f076ac
Madukaaa <madukakarunathilake2003@gmail.com>
2025-08-06 00:12:43 +0530

    Added the content

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,10 +1,9 @@
-'use client';
+"use client";
 
-import Navbar from '@/components/Navbar';
-import Image from 'next/image';
-import Footer from '@/components/Footer';
-import heroImage from '@/public/images/booking-hero.jpg'; 
-
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Footer from "@/components/Footer";
+import heroImage from "@/public/images/booking-hero.jpg";
 
 export default function AboutUsPage() {
   return (
@@ -22,7 +21,9 @@ export default function AboutUsPage() {
         />
         <div className="absolute inset-0 bg-gradient-to-b from-white via-white/30 to-transparent" />
         <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-5xl font-bold text-white drop-shadow-md text-center">Bookings</h1>
+          <h1 className="text-5xl font-bold text-white drop-shadow-md text-center">
+            Bookings
+          </h1>
         </div>
       </div>
 
@@ -31,8 +32,20 @@ export default function AboutUsPage() {
         Home &gt; <span className="text-black">Booking</span>
       </div>
 
-     <Footer />
-      
+      {/* Content Section */}
+      <div className="px-6 md:px-40 py-12 space-y-12">
+        <section>
+          <h2 className="text-3xl font-bold text-[#F68713] ">Bookings</h2>
+          <p className="text-gray-700 leading-relaxed text-justify pt-5">
+            Secure your spot on one of our unforgettable tours across Sri Lanka.
+            Choose your preferred package, travel dates, and group size and
+            leave the rest to us! Hassle-free booking, flexible options, and
+            instant confirmation.
+          </p>
+        </section>
+      </div>
+
+      <Footer />
     </>
   );
 }
