177ffee52b000434b9c91b1e9d1fa3ff98a4b0a3
AshenFerna29 <ashen2742@gmail.com>
2025-08-15 21:59:10 +0530

    scrolling animation

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,6 +6,7 @@ import ThingsToDo from "@/components/ThigsToDo";
 import Gallery from "@/components/Gallery";
 import PackageSection from "@/components/PackageSection";
 import Footer from "@/components/Footer";
+import VelocityMarqueeSection from "@/components/VelocityMarqueeSection";
 
 export default function HomePage() {
   return (
@@ -17,6 +18,7 @@ export default function HomePage() {
       <ThingsToDo />
       <PackageSection />
       <Gallery />
+       <VelocityMarqueeSection />
       <Footer />
     </>
   );
